486a137646639179500369a11bc477f15341afc8
Romeo <romeocorpuz808@gmail.com>
2018-06-08 21:50:15 -1000

    Submit

--- scope.js
+++ scope.js
@@ -2,11 +2,13 @@
 var isCookieJarOpen = null;
 
 function closeLid() {
-  /* answer here */
+  isCookieJarOpen = false;
+  
 }
 
 function openLid() {
-  /* answer here */
+  isCookieJarOpen = true;
+  
 }
 
 
@@ -17,7 +19,7 @@ function outerFunction() {
 
   function innerFunction() {
     var world = 'World';
-    return /* answer here */;
+    return hello + " " + world;
   }
   return innerFunction();
 }
@@ -31,8 +33,8 @@ function addMatrixElements(matrix) {
 
   for(var i = 0; i < matrix.length; i++) {
     /* fix counter variables in the second loop */
-    for(var i = 0; i < matrix[i].length; i++) {
-      result = result + matrix[i][i];
+    for(var j = 0; j < matrix[i].length; j++) {
+      result = result + matrix[i][j];
     }
   }
   return result;
@@ -54,8 +56,8 @@ function sendDataToClient() {
       authenticated: false
     };
 
-    if (userObject.handle === username) {
-      userObject.authenticated = true;
+    if (obj.handle === username) { //userObject should be changed to obj
+      obj.authenticated = true; //userObject should be changed to obj
       return userObject
     } else {
       return userObject
@@ -63,4 +65,4 @@ function sendDataToClient() {
   }
   authenticateUser(userObject, 'neo')
   return userObject
-}
+} 
